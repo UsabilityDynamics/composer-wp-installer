820048a91e2c8dd99a3f5a3b9907af9357825f24
Brad Jones <brad@kdis.com.au>
2014-04-16 15:45:37 +1000

    changes to installer

--- src/AssetMiniInstaller.php
+++ src/AssetMiniInstaller.php
@@ -2,11 +2,13 @@
 
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
 
 class AssetMiniInstaller extends LibraryInstaller
 {
-	public function install()
+	public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
 	{
+		parent::install($repo, $package);
 		echo "\n\n".'INSTALLING'."\n\n";
 	}
 	
