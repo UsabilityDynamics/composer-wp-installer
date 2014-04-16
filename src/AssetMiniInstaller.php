0491faa77898ee79332ef266284aaee6bddd8ad0
Brad Jones <brad@kdis.com.au>
2014-04-16 15:53:01 +1000

    yay the installer runs, now what data do i have???

--- src/AssetMiniInstaller.php
+++ src/AssetMiniInstaller.php
@@ -8,8 +8,13 @@ class AssetMiniInstaller extends LibraryInstaller
 {
 	public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
 	{
+		// Run the parent installer
 		parent::install($repo, $package);
+		
+		// Output some debug info
 		echo "\n\n".'INSTALLING'."\n\n";
+		print_r($repo);
+		print_r($package);
 	}
 	
 	public function supports($packageType)
